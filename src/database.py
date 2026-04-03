1b205cbfcda2d6b390596b914ab14c83c64e6bcb
Léo <cdf.leo.merkel@gmail.com>
2026-04-03 19:43:31 +0200

    compose for production

--- src/database.py
+++ src/database.py
@@ -1,12 +1,19 @@
+import os
 from sqlalchemy.exc import OperationalError
 from sqlmodel import create_engine
 from dotenv import dotenv_values
 
 config = dotenv_values(".env")
 
+db_user = os.getenv("DB_USER") or config.get("DB_USER")
+db_password = os.getenv("DB_PASSWORD") or config.get("DB_PASSWORD")
+db_address = os.getenv("DB_ADDRESS") or config.get("DB_ADDRESS")
+db_port = os.getenv("DB_PORT") or config.get("DB_PORT")
+db_name = os.getenv("DB_NAME") or config.get("DB_NAME")
+
 database_url = (
-    f"postgresql://{config['DB_USER']}:{config['DB_PASSWORD']}@"
-    f"{config['DB_ADDRESS']}:{config['DB_PORT']}/{config['DB_NAME']}"
+    f"postgresql://{db_user}:{db_password}@"
+    f"{db_address}:{db_port}/{db_name}"
 )
 engine = create_engine(database_url, echo=True)
 
